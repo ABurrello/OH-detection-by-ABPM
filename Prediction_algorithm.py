0de7377ed90c23a4e6ca0a99306af8174f90d20b
ABurrello <40060475+ABurrello@users.noreply.github.com>
2019-02-19 18:50:19 +0100

    Add files via upload

---
+++ Prediction_algorithm.py
@@ -0,0 +1,32 @@
+from sklearn.ensemble import RandomForestClassifier
+import scipy.io
+import numpy as np
+import pickle
+filename = 'Forest_model.sav'
+loaded_model = pickle.load(open(filename, 'rb'))
+print 'Welcome to the prediction tool: pls use the point as separator for decimal number: (3.7 and not 3,7)'
+print'-------------------------\n'
+Steroids= ['Daytime SBP [mmHg]',
+        'Daytime DBP [mmHg]',
+        'Daytime MBP [mmHg]',
+        'Nighttime SBP [mmHg]',
+        'Nighttime DBP [mmHg]',
+        'Nighttime MBP [mmHg]',
+        'Weighted BPV [%]',
+        'Daytime SBP Load [%]',
+        'Daytime DBP Load [%]',
+        'Nighttime SBP Load [%]',
+        'Nighttime DBP Load [%]',
+        'RD [0=No, 1=Yes]',
+        'PPH [0=No, 1=Yes]',
+        'Hypo-ep (Delta 15 / 24h) [number of episodes]',
+        'Hypo-aw (Delta 15 / 24h) []']
+Query = np.zeros((1,15))
+Prediction_type = ['OH (-)',
+'OH (+)']
+for i in range(len(Steroids)):
+    String_to_print = 'Insert value of ' +Steroids[i]+' and press enter:\n'
+    Query[0,i] = input(String_to_print)
+Prediction = loaded_model.predict(Query)
+print '------------'
+print 'The patient is predicted as ' + Prediction_type[Prediction[0]]
